e4ddb2676ecc39aec39463800154a679ec17fe50
zzerowin <alaykr123@gmail.com>
2022-03-22 21:52:37 +0900

    배열

--- 6_18 문법-배열/app.js
+++ 6_18 문법-배열/app.js
@@ -23,8 +23,35 @@ console.log(oneBooks);
 console.log(books);
 console.log(books.length);
 
-const twoBooks = books.slice(1, 2, "헴릿", "오셀로", "리어왕")
+const twoBooks = books.splice(1, 2, "헴릿", "오셀로", "리어왕")
 
 console.log(twoBooks);
 console.log(books);
 console.log(books.length);
+
+twoBooks.shift();
+
+console.log(twoBooks);
+
+twoBooks.unshift('한여름 밤의 꿈');
+
+console.log(twoBooks);
+
+const allBooks = books.join(' ');
+
+console.log(allBooks);
+
+const newBooks = allBooks.split(',')
+
+console.log(newBooks);
+
+console.log(oneBooks);
+console.log(twoBooks);
+
+const nextBooks = oneBooks.concat(twoBooks);
+
+console.log(nextBooks);
+
+const nextBookList = [...oneBooks, ...twoBooks];
+
+console.log(nextBookList);
