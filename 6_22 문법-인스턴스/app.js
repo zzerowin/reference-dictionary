627212902d67e61fab5cf8818d568cede6ba670a
zzerowin <alaykr123@gmail.com>
2022-03-25 22:03:59 +0900

    인스턴스

---
+++ 6_22 문법-인스턴스/app.js
@@ -0,0 +1,90 @@
+function CartV1() {
+  this.cart = [];
+  this.currentId = 0;
+}
+
+CartV1.prototype.getNewId = function () {
+  this.currentId++;
+  return this.currentId;
+};
+
+CartV1.createItem = function (name, price) {
+  return {
+    name, price
+  };
+};
+
+CartV1.prototype.addItem = function (item) {
+  this.cart.push({
+    ...item,
+    id: this.getNewId(),
+  });
+};
+
+CartV1.prototype.clearCart = function (item) {
+  this.cart = [];
+  this.currentId = 0;
+};
+
+const shoppingCarV1 = new CartV1();
+
+shoppingCarV1.addItem(CartV1.createItem('수박', 8000));
+shoppingCarV1.addItem(CartV1.createItem('사과', 12000));
+shoppingCarV1.addItem(CartV1.createItem('두부', 2000));
+
+console.log(shoppingCarV1.cart);
+
+
+class CartV2 {
+  static createItem = (name, price) => ({
+    name, price
+  });
+
+  cart;
+  currentId;
+
+  constructor() {
+    this.currentId = 0;
+    this.cart = [];
+  }
+
+  getNewId = () => {
+    this.currentId++;
+    return this.currentId
+  }
+
+  addItem = item => {
+    this.cart.push({
+      ...item,
+      id: this.getNewId(),
+    });
+  }
+
+  clearCart = () => {
+    this.currentId = 0;
+    this.cart = [];
+  }
+}
+
+const shoppingCartV2 = new CartV2();
+
+shoppingCartV2.addItem(CartV2.createItem('수박', 8000));
+shoppingCartV2.addItem(CartV2.createItem('사과', 12000));
+shoppingCartV2.addItem(CartV2.createItem('두부', 2000));
+
+console.log(shoppingCartV2.cart);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
