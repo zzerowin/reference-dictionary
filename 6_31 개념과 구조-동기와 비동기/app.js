36a63a1c8fdf7ab4cd3d850f85ebea0d88950441
zzerowin <alaykr123@gmail.com>
2022-03-30 21:02:22 +0900

    동기와 비동기(Promise)

---
+++ 6_31 개념과 구조-동기와 비동기/app.js
@@ -0,0 +1,40 @@
+const p = new Promise((resolve, reject) => {
+  // resolve('OK');
+  setTimeout(() => {
+    resolve('OK');
+  }, 2000)
+})
+
+p.then(function (ok) {
+  console.log('첫번째 성공');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('두번째 성공');
+    }, 3000)
+  })
+})
+  .then(function (ok) {
+    console.log(ok);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+function double(x) {
+  return x * 2;
+}
+
+function calcValue(a, b, cb) {
+  setTimeout(() => {
+    cb(a + b);
+  }, 100);
+}
+
+const x = double(100);
+const y = x;
+
+const r = calcValue(10, 20, (result) => {
+  console.log(result);
+});
+const z = r;
+
