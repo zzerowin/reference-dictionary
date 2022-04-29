bacdd10ee22ac70f2f5d20f5664bd42aac9fa217
zzerowin <alaykr123@gmail.com>
2022-04-29 21:55:48 +0900

    크롬 개발자 도구

---
+++ 6_46 도구-디버거-크롱 개발자 도구/app.js
@@ -0,0 +1,25 @@
+function salePrice(discountRate, price) {
+  return price - (price * (discountRate * 0.01));
+}
+
+console.log('여름 세일 - ' + salePrice(30, 567000));
+console.log('겨울 세일 - ' + salePrice(10, 567000));
+console.log('\n');
+
+function discountPrice(discountRate) {
+  return function (price) {
+    return price - (price * (discountRate * 0.01));
+  }
+}
+
+debugger;
+console.log('여름 세일 - ' + discountPrice(30)(567000));
+console.log('겨울 세일 - ' + discountPrice(10)(567000));
+console.log('\n');
+
+let summerPrice = discountPrice(30);
+let winterPrice = discountPrice(10);
+
+
+console.log('여름 세일 - ' + summerPrice(567000));
+console.log('겨울 세일 - ' + winterPrice(567000));
