493a0c46b0c368ca7d2a8b6367a712057fa1a1a7
zzerowin <alaykr123@gmail.com>
2022-08-25 13:30:23 +0900

    문법-식별자

--- 6_01 문법-식별자/app.js
+++ 6_01 문법-식별자/app.js
@@ -1,20 +1,15 @@
+
 let age = 10;
 const AGE = 10;
 
-function setAge(){
+function setAge() {
 
 }
 
 const o = {
-    age : 10,
-    ['myName']: '김',
-    ['123my Name']: '김'
+    age: 10,
+    ['123my Name']: '박',
 }
 
-o.age
-o['myName']
-o['123my Name']
-
-
-// setAge (카멜 케이스)
-// set_Age (스네이크 케이스)
+o['123 Age']
+console.log(o['123my Name']);
