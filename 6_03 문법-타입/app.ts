561e036e541d72a0a89ffa6eba51231b42cf5788
zzerowin <alaykr123@gmail.com>
2022-08-25 14:14:59 +0900

    타입

--- 6_03 문법-타입/app.ts
+++ 6_03 문법-타입/app.ts
@@ -4,7 +4,6 @@ function addAge(age: number): number {
   return age + 1;
 }
 
-let age: number = addAge(30);
-
-console.log(age);
-
+// '30' X -> 30 number O
+let age: number = addAge(55);
+console.log(age)
