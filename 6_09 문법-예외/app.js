feaa08424225eec85bf1743984c8b3c507da6174
zzerowin <alaykr123@gmail.com>
2022-08-26 09:26:29 +0900

    예외

--- 6_09 문법-예외/app.js
+++ 6_09 문법-예외/app.js
@@ -1,6 +1,6 @@
 
 function doException() {
-  throw new Error('와우! 오류야 ㅋㅋ');
+  throw new Error('와우! 오류야!');
 }
 
 function noException() {
@@ -11,20 +11,20 @@ function callException(type) {
   if (type === 'do') {
     doException();
   } else {
-
     if (noException() === false) {
       console.log('오류처리');
-    };
+    }
   }
 }
 
 function main() {
   try {
     callException('do');
-  } catch(e) {
+    console.log('a');
+  } catch (e) {
     console.log(e);
   } finally {
-    console.log('done');
+    console.log('완료');
   }
 }
 
