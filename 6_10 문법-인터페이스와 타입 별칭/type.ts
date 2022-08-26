d48d4a8fe917480e3bbff0925810bc25fa1f3548
zzerowin <alaykr123@gmail.com>
2022-08-26 11:23:29 +0900

    인터페이스와 타입 별칭

--- 6_10 문법-인터페이스와 타입 별칭/type.ts
+++ 6_10 문법-인터페이스와 타입 별칭/type.ts
@@ -1,102 +1,100 @@
 export type YesOrNo = 'Y' | 'N';
 export type DayOfWeek = '월' | '화' | '수' | '목' | '금' | '토' | '일';
-export enum DayOfTheWeek {'월', '화', '수', '목', '금', '토','일'};
 
+export enum DayOfTheWeek {'월', '화', '수', '목', '금', '토', '일'};
 
 export type Name = string;
 export type Email = string;
 export type FooFunction = () => string;
 
 export interface IUser {
-    readonly id : number;
-    readonly name : Name;
-    email: string;
-    receiveInfo: boolean;
-    active : YesOrNo; 
+  readonly id: number;
+  readonly name: Name;
+  email: string;
+  receiveInfo: boolean;
+  active: YesOrNo;
 }
 
-export interface IUser{
-    address?: string;
+export interface IUser {
+  address?: string;
 }
 
 export type TUser = {
-    readonly id : number;
-    readonly name : string;
-    email: Email;
-    receiveInfo : boolean;
-    active : YesOrNo;
+  readonly id: number;
+  readonly name: string;
+  email: Email;
+  receiveInfo: boolean;
+  active: YesOrNo;
 }
 
 // export type TUser = {
 //     address?: string;
 // }
 
-export interface IUserProfile extends IUser{
-    profileImage: string;
-    github?: string;
-    twitter?: string;
+export interface IUserProFile extends IUser {
+  profileImage: string;
+  github?: string;
+  twitter?: string;
 }
 
 export type TUserProfile = IUser & {
-    profileImage: string;
-    github?: string;
-    twitter?: string;
+  profileImage: string;
+  github?: string;
+  twitter?: string;
 }
 
 export interface Color {
-    fontColor: string;
-    strokeColor: string;
-    borderColor: string;
-    backgroundColor : string;
+  fontColor: string;
+  strokeColor: string;
+  bordColor: string;
+  backgroundColor: string;
 }
 
 export type Display = {
-    display: 'none' | 'block';
-    visibility: boolean;
-    opacity : number;
+  display: 'none' | 'block';
+  visiblity: boolean;
+  opacity: number;
 }
 
 export type Geometry = {
-    width:number;
-    height:number;
-    padding:number;
-    margin:number;
+  width: number;
+  height: number;
+  padding: number;
+  margin: number;
 }
 
-export interface IStyle extends Color, Display, Geometry{
-    tagName : string;
+export interface IStyle extends Color, Display, Geometry {
+  tagName: string;
 }
 
 export type TStyle = Color & Display & Geometry & {
-    tagName : string;
+  tagName: string;
 }
 
-export interface IOnlyNumberValueObject  {
-    [key:string]:number;
+export interface IOnlyNumberValueObject {
+  [key: string]: number;
 }
 
 export type TOnlyBooleanValueObject = {
-    [prop:string]: boolean;
+  [prop: string]: boolean;
 }
 
 export interface IGetApi {
-    (url:string, search?: string): Promise<string>
+  (url: string, search?: string): Promise<string>;
 }
 
 export type TGetApi = {
-    (url:string, search?: string): Promise<string>
+  (url: string, search?: string): Promise<string>;
 }
 
 export interface IRect {
-    id: number;
-    x: number;
-    y: number;
-    width: number;
-    height: number;
+  id: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
 
 export interface IRectConstruct {
-    new (x: number, y: number, width:number, height:number): IRect;
+  new(x: number, y: number, width: number, height: number): IRect;
 }
-
-
