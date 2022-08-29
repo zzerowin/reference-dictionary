9bf3b9e714b5e7adf8dd69da7621def77737f3df
zzerowin <alaykr123@gmail.com>
2022-08-29 13:13:41 +0900

    프로그래밍 도구로서의 객체

--- 6_17 문법-객체-프로그래밍 도구로서의 객체/app.js
+++ 6_17 문법-객체-프로그래밍 도구로서의 객체/app.js
@@ -1,40 +1,41 @@
-function calculateCircleArea(radius){
-  return radius * radius * Math.PI;
+const PI = Math.PI;
+
+function calculateCircleArea(radius) {
+  return radius * radius * PI;
 }
 
-function calculateRectArea(width, height){
+function calculateRectArea(width, height) {
   return width * height;
 }
 
-// circle 클래스
 class Circle {
   #radius;
 
-  constructor(radius){
+  constructor(radius) {
     this.#radius = radius;
   }
-  get radius(){
+
+  get radius() {
     return this.#radius;
   }
 
-  area = () => this.#radius * this.#radius * Math.PI;
+  area = () => this.#radius * this.#radius * PI;
 }
 
-// rect 클래스
 class Rect {
   #width;
   #height;
 
-  constructor(width, height){
+  constructor(width, height) {
     this.#width = width;
     this.#height = height;
   }
 
-  get width(){
+  get width() {
     return this.#width;
   }
 
-  get height(){
+  get height() {
     return this.#height;
   }
 
@@ -42,11 +43,10 @@ class Rect {
 }
 
 const circle = new Circle(50);
-const rect = new Rect(150,200);
-
+const rect = new Rect(150, 300);
 
 console.log(calculateCircleArea(circle.radius));
 console.log(calculateRectArea(rect.width, rect.height));
 
-console.log(circle.area())
-console.log(rect.area())
+console.log(circle.area());
+console.log(rect.area());
