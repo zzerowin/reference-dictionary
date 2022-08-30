28845e4df0d8c686edbf6adc06acb171ae606a1e
zzerowin <alaykr123@gmail.com>
2022-08-30 11:12:10 +0900

    배열 연산

--- 6_19 문법-배열 연산/index.ts
+++ 6_19 문법-배열 연산/index.ts
@@ -2,35 +2,39 @@ type Book = {
   title: string;
   copyright?: string;
   author?: string;
-}
+};
 
-const books: string[] = ["헨리 6세", "리처드 3세", "실수 연발", "말광량이 길들이기", "헨리 8세"];
+const books: string[] = [
+  "헨리 6세",
+  "리처드 3세",
+  "실수 연발",
+  "말괄량이 길들이기",
+  "헨리 8세",
+];
 
-books.forEach((book: string, idx: number) => {
+books.forEach((book: string, idx: number, books: string[]) => {
   console.log(book, idx);
-});
+})
 
-const bookObject: Book[] = books.map((book: string) => {
+
+const bookObject: Book[] = books.map((book:string) =>{
   return {
-    title: book,
-    author: undefined
+    title:book,
+    author: undefined,
   };
 });
-
 console.log(bookObject);
 
-const ShakespeareOneBooks: Book[] = books
-  .map((book: string) => ({
-    title:book
-  }))
+const ShakespeareOneBooks: Book[] = books.map((book: string) => ({
+  title:book
+}))
   .map((book: Book) => ({
     ...book,
     author: "William Shakespeare"
   }));
-
 console.log(ShakespeareOneBooks);
 
-const bookTitleToBookObject = (book: string) => ({title: book})
+const bookTitleToBookObject = (book: string) => ({title: book});
 const makeAuthor = (name: string) => (book: Book) => ({
   ...book,
   author: name
@@ -39,73 +43,41 @@ const makeAuthor = (name: string) => (book: Book) => ({
 const shakespeareTwoBooks: Book[] = books
   .map(bookTitleToBookObject)
   .map(makeAuthor("William Shakespeare"));
-
 console.log(shakespeareTwoBooks);
 
-// @ts-ignore
-const henry: Book[] = shakespeareTwoBooks.filter((book: Book) => book.title.includes("헨리"));
-
+const henry: Book[] = shakespeareTwoBooks.filter((book: Book) =>
+  book.title.includes('헨리')
+);
 console.log(henry);
 
 const someNumbers: number[] = [10, 5, 3, 14, 56];
-
 const sumNumber = someNumbers.reduce((a: number, b: number) => a + b, 0);
-
 console.log(sumNumber);
 
 type SomeObject = {
   [key: string]: string | number;
-};
+}
 
 const someObjects: SomeObject[] = [
-  {border: "none"},
+  {border: 'none'},
   {fontSize: 24},
-  {className: "box sm-box"}
+  {className: 'box sm-box'}
 ];
 
 const someObject: SomeObject = someObjects.reduce(
   (a: SomeObject, b: SomeObject) => ({...a, ...b}),
   {}
 );
-
 console.log(someObject);
 
 function sumNumbers(): number {
-  // @ts-ignore
   return Array.from(arguments).reduce((a: number, b: number) => a + b, 0);
 }
 
-// @ts-ignore
-console.log(sumNumbers(10, 20, 30, 40, 50));
+// console.log(sumNumbers(10, 20, 30, 40, 50));
 
 function sumNumbersForTypeScript(...args: number[]): number {
   return args.reduce((a: number, b: number) => a + b, 0);
 }
 
 console.log(sumNumbersForTypeScript(10, 20, 30, 40, 50));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
