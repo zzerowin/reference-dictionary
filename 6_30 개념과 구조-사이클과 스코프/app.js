aa474c7103d330a2f260d5569247ace7bd472058
zzerowin <alaykr123@gmail.com>
2022-08-31 14:36:54 +0900

    사이클과 스코프

--- 6_30 개념과 구조-사이클과 스코프/app.js
+++ 6_30 개념과 구조-사이클과 스코프/app.js
@@ -1,12 +1,13 @@
-let myName = 'park';
+
+let myName = 'Park';
 
 function foo() {
   let x = 10;
 
   console.log(myName);
   console.log(x);
-  bar();
-  zoo();
+  bar(); // 호이스팅
+  // zoo(); // 함수문이 아니라 함수식이기 때문에 호이스팅 불가 X
 
   function bar() {
     let y = 10;
@@ -15,20 +16,19 @@ function foo() {
     console.log(myName);
   }
 
-  const zoo = function () {
-    return null;
-  };
+  const zoo = () => {
 
-  // console.log(y);
+  }
+
+  // console.log(y); // X
 
   if (x === 10) {
     let x = 100;
 
     console.log(x);
   }
-
   bar();
 }
 
 foo();
-console.log(x);
+// console.log(x); // X
