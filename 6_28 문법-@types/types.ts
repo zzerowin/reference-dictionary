e73afab6efb5db17fef7bd49223539c2c207ae09
zzerowin <alaykr123@gmail.com>
2022-08-31 14:13:34 +0900

    @types

--- 6_28 문법-@types/types.ts
+++ 6_28 문법-@types/types.ts
@@ -1,14 +1,13 @@
-// @ts-ignore
 import { v4 } from 'uuid';
 
 type UniqObject = {
   id: string;
   [key: string]: string | number | boolean;
-}
+};
 
 const makeObject = (): UniqObject => ({
   id: v4(),
-});
+})
 
 console.log(makeObject());
 console.log(makeObject());
