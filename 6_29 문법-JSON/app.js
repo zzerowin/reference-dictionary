b40cafa4b7f3bf30fe4c793741c6bd5d36214c22
zzerowin <alaykr123@gmail.com>
2022-08-31 14:22:14 +0900

    JSON

--- 6_29 문법-JSON/app.js
+++ 6_29 문법-JSON/app.js
@@ -1,6 +1,8 @@
+
+// 함수 X JSON은 객체이기 때문에 문자 | 배열 | 숫자 | 객체
 const jsonString = `
   {
-    "name": "Park young seung",
+    "name": "박영승",
     "age": 25,
     "bloodType": "A"
   }
@@ -13,7 +15,5 @@ try {
 
   console.log(JSON.stringify(myJson));
 } catch (e) {
-  console.log('다시한번 시도해 주세요.');
+  console.log('다시한번 시도해주세요.');
 }
-
-
