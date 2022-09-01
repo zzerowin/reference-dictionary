3d9842601e79178151e667dcac97d78d03d32166
zzerowin <alaykr123@gmail.com>
2022-09-01 11:26:41 +0900

    문자열을 형태가 다른 문자열로 변환하기

--- 6_41 프로세싱-형태의 변환-문자열을 형태가 다른 문자열로 변환하기/app.js
+++ 6_41 프로세싱-형태의 변환-문자열을 형태가 다른 문자열로 변환하기/app.js
@@ -1,16 +1,15 @@
-// 문자열을 형태가 다른 문자열로 변환하기
 
 const simpleCamel = (text, splitter = ' ') => text.split(splitter)
-  .map((word, wi) => word.split('')
-    .map((c, ci) => wi > 0 && ci === 0 ? c.toUpperCase() : c.toLowerCase())
-    .join(''))
-  .join('');
+                    .map((word, wi) => word.split('')
+                      .map((c, ci) => wi > 0 && ci === 0 ? c.toUpperCase() : c.toLowerCase())
+                      .join(''))
+                    .join('');
 
 function convertCamelName(name) {
   let camelName = '';
 
   for (let i = 0, newSpace = false; i < name.length; i++) {
-    if (name[i] === ' ') {
+    if (name[i] == ' ') {
       newSpace = true;
       continue;
     }
@@ -26,8 +25,8 @@ function convertCamelName(name) {
   return camelName;
 }
 
-const camelName1 = convertCamelName('park zero win');
-const camelName2 = simpleCamel('PARK ZERO WIN');
+const camelName1 = convertCamelName('park young seung');
+const camelName2 = simpleCamel('PARK YOUNG SEUNG');
 
 console.log(camelName1);
 console.log(camelName2);
