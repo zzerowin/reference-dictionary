5e168f98f51b842266af75e729308b90652b39a7
zzerowin <alaykr123@gmail.com>
2022-09-01 11:11:11 +0900

    객체를 문자열로 변환하기

--- 6_40 프로세싱-형태의 변환-객체를 문자열로 변환하기/app.js
+++ 6_40 프로세싱-형태의 변환-객체를 문자열로 변환하기/app.js
@@ -3,13 +3,13 @@
  */
 
 const cartItems = [
-  { id: 1, item: '핸드밀', price: 40000, discount: 0},
-  { id: 2, item: 'A4용지', price: 4000, discount: 0},
-  { id: 3, item: '수영복', price: 120000, discount: 0},
-  { id: 4, item: '색연필72색', price: 150000, discount: 0},
+  { id: 1, item: '핸드밀', price: 40000, discount: 0 },
+  { id: 2, item: 'A4용지', price: 4000, discount: 0 },
+  { id: 3, item: '수영복', price: 120000, discount: 0 },
+  { id: 4, item: '색연필72색', price: 150000, discount: 0 },
 ];
 
-const cartItemArray = [];
+const cartItemsArray = [];
 
 for (const item of cartItems) {
   const row = [];
@@ -18,17 +18,17 @@ for (const item of cartItems) {
     row.push(value);
   }
 
-  cartItemArray.push(row.join());
+  cartItemsArray.push(row.join());
 }
 
-console.log(cartItemArray.join('==='));
+console.log(cartItemsArray.join(' === '));
 
 const extractValueInObject = obj => Object
   .entries(obj)
   .map(([, value]) => String(value));
 
-const cartItemsString = cartItems
+const cartItemString = cartItems
   .map(extractValueInObject)
-  .join('===');
+  .join(' === ');
 
-console.log(cartItemsString);
+console.log(cartItemString);
