8273ecd945653b7c87d3c8371e0bf68a77f5bfb7
zzerowin <alaykr123@gmail.com>
2022-09-01 11:47:20 +0900

    문자열 변환 고급 기법 - 템플릿

--- 6_42 프로세싱-형태와변환-문자열 변환 고급 기법-템플릿/app.js
+++ 6_42 프로세싱-형태와변환-문자열 변환 고급 기법-템플릿/app.js
@@ -1,26 +1,23 @@
+
 const userName = 'Park youngseung';
 const bolder = text => `<b>${text}</b>`;
 
-console.log(`
-  Hi~ ${userName}
-`);
+console.log(`Hello~ ${userName}`);
 
-console.log(`
-  Hi~ ${bolder(userName)}
-`);
+console.log(`Hello~ ${bolder(userName)}`);
 
 function div(strings, ...fns) {
   const flat = s => s.split('\n').join('');
 
   return function (props) {
-    return `<div style="${flat(strings[0]) + (fns[0] && fns[0](props)) + flat(strings[1])}"></div>`;
+    return `<div style="${flat(strings[0] + (fns[0] && fns[0](props)) + flat(strings[1]))}"></div>`;
   }
 }
 
 const Div = div`
-  font-size" 20px;
-  color; ${props => props.active ? 'white' : 'gray'};
+  font-size: 20px;
+  color: ${props => props.active ? 'white' : 'gray'};
   border: none;
 `;
 
-console.log(Div({active: true}));
+console.log(Div({ active: true }))
